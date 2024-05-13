e9994ca2a7423dfaf82f64b2f8deb9e55370bdc3
Pouria Shirali <impoush@gmail.com>
2024-05-13 11:29:37 +0330

    add image and fonts to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gympedia/common/color_extentions.dart';
 import 'package:gympedia/view/on_boarding/on_boarding_view.dart';
 
 void main() {
@@ -14,21 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GymPedia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        primaryColor: CustomColor.primaryColor1,
+        fontFamily: 'Iran Bakh ',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),

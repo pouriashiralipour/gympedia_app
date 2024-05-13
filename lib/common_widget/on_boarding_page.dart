1dfae1a804f249e38c76df03ba7d1f6ab3b14640
Pouria Shirali <impoush@gmail.com>
2024-05-13 14:53:48 +0330

    create list for onboarding objects

---
+++ lib/common_widget/on_boarding_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gympedia/common/color_extentions.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  final Map pObj;
+  const OnBoardingPage({super.key, required this.pObj});
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return SizedBox(
+      height: media.height,
+      width: media.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SvgPicture.asset(
+            pObj["image"].toString(),
+            width: media.width,
+            fit: BoxFit.fitWidth,
+          ),
+          SizedBox(
+            height: media.width * 0.1,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Text(
+              pObj['title'].toString(),
+              style: TextStyle(
+                  color: CustomColor.black,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Poopins',
+                  fontSize: 24),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Text(
+              pObj['subtitle'].toString(),
+              style: TextStyle(
+                  fontFamily: 'Poopins', fontSize: 14, color: CustomColor.gray),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/on_boarding/on_boarding_view.dart
+++ lib/view/on_boarding/on_boarding_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:gympedia/common/color_extentions.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gympedia/common_widget/on_boarding_page.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -12,6 +13,32 @@ class OnBoardingView extends StatefulWidget {
 
 class _OnBoardingViewState extends State<OnBoardingView> {
   PageController controller = PageController();
+  List pageList = [
+    {
+      "title": "Track Your Goal",
+      "subtitle":
+          "Don't worry if you have trouble determining your goals, We can help you determine your goals and track your goals",
+      "image": "assets/img/on_1.svg",
+    },
+    {
+      "title": "Get Burn",
+      "subtitle":
+          "Let’s keep burning, to achive yours goals, it hurts only temporarily, if you give up now you will be in pain forever",
+      "image": "assets/img/on_2.svg",
+    },
+    {
+      "title": "Eat Well",
+      "subtitle":
+          "Let's start a healthy lifestyle with us, we can determine your diet every day. healthy eating is fun",
+      "image": "assets/img/on_3.svg",
+    },
+    {
+      "title": "Improve Sleep  Quality",
+      "subtitle":
+          "Improve the quality of your sleep with us, good quality sleep can bring a good mood in the morning",
+      "image": "assets/img/on_4.svg",
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
@@ -21,48 +48,9 @@ class _OnBoardingViewState extends State<OnBoardingView> {
         children: <Widget>[
           PageView.builder(
             controller: controller,
+            itemCount: pageList.length,
             itemBuilder: (context, index) {
-              return SizedBox(
-                height: media.height,
-                width: media.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/img/on_1.svg',
-                      width: media.width,
-                      fit: BoxFit.fitWidth,
-                    ),
-                    SizedBox(
-                      height: media.width * 0.1,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Text(
-                        'اهدافت رو دنبال کن',
-                        textDirection: TextDirection.rtl,
-                        style: TextStyle(
-                            color: CustomColor.black,
-                            fontWeight: FontWeight.w900,
-                            fontFamily: 'IranBakh',
-                            fontSize: 24),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Text(
-                        'نگران نباش اگه تو برنامه ریزی مشکل داری ، تیم ما میتونه بهتون کمک کنه تا به بدن ایده آل خودتون برسین',
-                        textDirection: TextDirection.rtl,
-                        style: TextStyle(
-                            fontFamily: 'IranYekan',
-                            fontSize: 16,
-                            color: CustomColor.gray),
-                      ),
-                    ),
-                  ],
-                ),
-              );
+              return OnBoardingPage(pObj: pageList[index]);
             },
           )
         ],

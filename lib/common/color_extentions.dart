3bbc20ff6557ae27ae5bb8460d1032bceecf25a0
Pouria Shirali <impoush@gmail.com>
2024-05-13 11:10:04 +0330

    add promary and secondary color

---
+++ lib/common/color_extentions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static Color get primaryColor1 => const Color(0xff93A3FD);
+  static Color get primaryColor2 => const Color(0xff9DCEFF);
+
+  static Color get secondryColor1 => const Color(0xffC58BF2);
+  static Color get secondryColor2 => const Color(0xffEEA4CE);
+}

49504e04771f0c0ff6906df37dc3a7476203c196
Pouria Shirali <impoush@gmail.com>
2024-05-13 11:49:25 +0330

    add ob boarding page

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GymPedia',
       theme: ThemeData(
         primaryColor: CustomColor.primaryColor1,
--- lib/view/on_boarding/on_boarding_view.dart
+++ lib/view/on_boarding/on_boarding_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gympedia/common/color_extentions.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -8,8 +9,35 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  PageController controller = PageController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: CustomColor.white,
+      body: Stack(
+        children: <Widget>[
+          PageView.builder(
+            controller: controller,
+            itemBuilder: (context, index) {
+              return SizedBox(
+                height: media.height,
+                width: media.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/img/on_1.png',
+                      width: media.width,
+                      fit: BoxFit.fitWidth,
+                    )
+                  ],
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
